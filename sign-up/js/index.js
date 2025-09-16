b6d68954179e31b5beed5e4d201baa23bac0315d
Rana-Ahmed24 <ra3936299@gmail.com>
2025-09-16 17:16:57 +0300

    Fix signup redirect to login.html

--- sign-up/js/index.js
+++ sign-up/js/index.js
@@ -11,78 +11,67 @@ let existMsg = document.getElementById("existMsg");
 
 let userList = [];
 
-
-
-
 if (localStorage.getItem("users")) {
-    userList = JSON.parse(localStorage.getItem("users"));
+  userList = JSON.parse(localStorage.getItem("users"));
 }
-
 
 form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    if (validateAllInputs(signupName, nameMsg) && validateAllInputs(signupEmail, emailMsg) && validateAllInputs(signupPassword, passwordMsg)) {
-        addUser();
-        form.reset();    }
-}
-)
-
-
-
-signInLink.addEventListener("click", function (e) {
-    window.location.href = "../sign-in/login.html";
+  e.preventDefault();
+  if (
+    validateAllInputs(signupName, nameMsg) &&
+    validateAllInputs(signupEmail, emailMsg) &&
+    validateAllInputs(signupPassword, passwordMsg)
+  ) {
+    addUser();
+    form.reset();
+  }
 });
 
+signInLink.addEventListener("click", function (e) {
+  window.location.href = "../sign-in/login.html";
+});
 
+function validateAllInputs(element, alertMsg) {
+  let regex = {
+    signupName: /^[A-Za-z\s]{3,}$/,
+    signupEmail: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    signupPassword: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+  };
 
+  let text = element.value;
 
-
-
-
-function validateAllInputs(element,alertMsg) {
-    let regex = {
-      signupName: /^[A-Za-z\s]{3,}$/,
-      signupEmail: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-      signupPassword: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
-    };
-
-    let text = element.value
-    
-    if (regex[element.id].test(text)) {
-        alertMsg.classList.replace("d-block", "d-none");
-        return true;
-    }
-    else {
-        alertMsg.classList.replace("d-none", "d-block");
-        return false;
-    }
-
+  if (regex[element.id].test(text)) {
+    alertMsg.classList.replace("d-block", "d-none");
+    return true;
+  } else {
+    alertMsg.classList.replace("d-none", "d-block");
+    return false;
+  }
 }
 
 function addUser() {
-    if (!isExistingUser(signupEmail.value)) {
-        let user = {
-        name: signupName.value,
-        email: signupEmail.value,
-        password: signupPassword.value
+  if (!isExistingUser(signupEmail.value)) {
+    let user = {
+      name: signupName.value,
+      email: signupEmail.value,
+      password: signupPassword.value,
     };
     userList.push(user);
-        localStorage.setItem("users", JSON.stringify(userList));
-        existMsg.classList.replace("d-block", "d-none");
-        setTimeout(function () {
-            window.location.href = "../../sign in/login.html";
-        }, 2000);
-    }
-    else {
-        existMsg.classList.replace("d-none", "d-block");
-    }
+    localStorage.setItem("users", JSON.stringify(userList));
+    existMsg.classList.replace("d-block", "d-none");
+    setTimeout(function () {
+      window.location.href = "../sign-in/login.html";
+    }, 2000);
+  } else {
+    existMsg.classList.replace("d-none", "d-block");
+  }
 }
 
 function isExistingUser(userEmail) {
-   for (let i = 0; i < userList.length; i++) {
-       if (userList[i].email === userEmail) {
-           return true;
-       }
-   }
-   return false;
+  for (let i = 0; i < userList.length; i++) {
+    if (userList[i].email === userEmail) {
+      return true;
+    }
+  }
+  return false;
 }
